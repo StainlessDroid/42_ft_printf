3909f2df766896943d63a257d7633549f793bab1
Manuel Pascual vazquez <mpascual@c3r10s6.42madrid.com>
2020-09-23 17:30:51 +0200

    Revert "file reorganization II"
    This reverts commit 75dd3f5e29d3c29e6b2f753f8f8887f9d974dc08.

---
+++ src/flags.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   flags.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpascual <mpascual@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/14 17:08:59 by mpascual          #+#    #+#             */
+/*   Updated: 2020/09/22 19:05:15 by mpascual         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../header.h"
+
+void init_flags(s_var *var)
+{
+	var->printed_chars = 0;
+	var->precision = 0;
+	var->width = 0;
+	var->minus = FALSE;
+	var->zero = FALSE;
+	var->point = FALSE;
+	var->asterisk = FALSE;
+	var->error = FALSE;
+}
+
+int     is_flag(char c)
+{
+    if (c == '-' || c == '.' || c == '*' || ft_isdigit(c))
+        return (1);
+    else
+        return (0);    
+}
+
+int		is_type(char c)
+{
+	if (c == 'c' ||  c == 's' || c == 'i' || c == 'd' || c == 'u' || c == 'x'
+		|| c == 'X' || c == 'p')
+		return (1);
+	else
+		return (0);
+}
