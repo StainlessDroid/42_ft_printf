a9f6945561b91718f671ad3579e0d0e45d11e169
Manuel Pascual vazquez <mpascual@c1r1s5.42madrid.com>
2020-11-13 12:18:32 +0100

    modifications to run in guacamole

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mpascual <mpascual@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/14 17:17:37 by mpascual          #+#    #+#             */
-/*   Updated: 2020/11/13 03:08:05 by mpascual         ###   ########.fr       */
+/*   Updated: 2020/11/13 12:14:38 by mpascual         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -104,7 +104,7 @@ void		check_type(const char c, va_list arg, t_var *var)
 	else if (c == 'X')
 		print_x(va_arg(arg, unsigned int), TRUE, var);
 	else if (c == '%')
-		var->printed_chars += ft_putchar('%');
+		var->printed_chars += print_c('%', var);
 }
 
 int			ft_printf(const char *format, ...)
